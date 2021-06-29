a9f70f714f771cedeb99f2bf817712453c98748d
YouJeen-ru <1.rouch@mail.ru>
2021-06-29 11:54:55 +0300

    ballMove, computerAI, animate, startGame, restart , show who won realized

--- script.js
+++ script.js
@@ -46,6 +46,8 @@ if (isMobile.matches) {
 let playerScore = 0
 let computerScore = 0
 const winningScore = 7
+let isGameOver = true;
+let isNewGame = true;
 
 
 // Render Everything on Canvas
@@ -91,5 +93,157 @@ function createCanvas() {
   renderCanvas()
 }
 
-createCanvas()
+// Reset Ball to Center
+function ballReset() {
+  ballX = width / 2
+  ballY = height / 2
+  speedY = -3
+  paddleContact = false
+}
 
+// Adjust Ball Movement
+function ballMove() {
+  // Vertical Speed
+  ballY += -speedY
+  // Horizontal Speed
+  if (playerMoved && paddleContact) {
+    ballX += speedX
+  }
+}
+
+// Determine What Ball Bounces Off, Score Points, Reset Ball
+function ballBoundaries() {
+  // Bounce off Left Wall
+  if (ballX < 0 && speedX < 0) {
+    speedX = -speedX
+  }
+  // Bounce off Right Wall
+  if (ballX > width && speedX > 0) {
+    speedX = -speedX
+  }
+  // Bounce off player paddle (bottom)
+  if (ballY > height - paddleDiff) {
+    if (ballX > paddleBottomX && ballX < paddleBottomX + paddleWidth) {
+      paddleContact = true
+      // Add Speed on Hit
+      if (playerMoved) {
+        speedY -= 1
+        // Max Speed
+        if (speedY < -5) {
+          speedY = -5
+          computerSpeed = 6
+        }
+      }
+      speedY = -speedY
+      trajectoryX = ballX - (paddleBottomX + paddleDiff)
+      speedX = trajectoryX * 0.3
+    } else if (ballY > height) {
+      // Reset Ball, add to Computer Score
+      ballReset()
+      computerScore++
+    }
+  }
+  // Bounce off computer paddle (top)
+  if (ballY < paddleDiff) {
+    if (ballX > paddleTopX && ballX < paddleTopX + paddleWidth) {
+      // Add Speed on Hit
+      if (playerMoved) {
+        speedY += 1
+        // Max Speed
+        if (speedY > 5) {
+          speedY = 5
+        }
+      }
+      speedY = -speedY
+    } else if (ballY < 0) {
+      // Reset Ball, add to Player Score
+      ballReset()
+      playerScore++
+    }
+  }
+}
+
+// Computer Movement
+function computerAI() {
+  if (playerMoved) {
+    if (paddleTopX + paddleDiff < ballX) {
+      paddleTopX += computerSpeed
+    } else {
+      paddleTopX -= computerSpeed
+    }
+  }
+}
+
+function showGameOverEl(winner) {
+  // Hide Canvas
+  canvas.hidden = true
+  // Container
+  gameOverEl.textContent = ''
+  gameOverEl.classList.add('game-over-container')
+  // Title
+  const title = document.createElement('h1')
+  title.textContent = `${winner} Wins!`
+  // Button
+  const playAgainBtn = document.createElement('button')
+  playAgainBtn.setAttribute('onclick', 'startGame()')
+  playAgainBtn.textContent = 'Play Again'
+  // Append
+  gameOverEl.append(title, playAgainBtn)
+  body.appendChild(gameOverEl)
+
+
+}
+
+// Check If One Player Has Winning Score, If They Do, End Game
+function gameOver() {
+  if (playerScore === winningScore || computerScore === winningScore) {
+    isGameOver = true
+    // Set Winner
+    let winner = playerScore === winningScore ? 'Player 1' : 'Computer'
+    showGameOverEl(winner)
+  }
+}
+
+// Called Every Frame
+function animate() {
+  renderCanvas()
+  ballMove()
+  ballBoundaries()
+  computerAI()
+  gameOver()
+  if (!isGameOver) {
+    window.requestAnimationFrame(animate)
+  }
+
+}
+
+// Start Game, Reset Everything
+function startGame() {
+  if (isGameOver && !isNewGame) {
+    body.removeChild(gameOverEl)
+    canvas.hidden = false
+
+  }
+  isGameOver = false
+  isNewGame = false
+  playerScore = 0
+  computerScore = 0
+  ballReset()
+  createCanvas()
+  animate()
+  canvas.addEventListener('mousemove', (e) => {
+    playerMoved = true
+    // Compensate for canvas being centered
+    paddleBottomX = e.clientX - canvasPosition - paddleDiff
+    if (paddleBottomX < paddleDiff) {
+      paddleBottomX = 0
+    }
+    if (paddleBottomX > width - paddleWidth) {
+      paddleBottomX = width - paddleWidth
+    }
+    // Hide Cursor
+    canvas.style.cursor = 'none'
+  })
+}
+
+startGame()
